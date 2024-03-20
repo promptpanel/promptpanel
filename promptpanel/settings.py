52a8467864b6467c897304e6d99b22a7a9b78961
Martin Mark <martin@promptpanel.com>
2024-03-20 10:23:22 -0400

    Database init

--- promptpanel/settings.py
+++ promptpanel/settings.py
@@ -146,7 +146,10 @@ WSGI_APPLICATION = "promptpanel.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "database/db.sqlite3",
+    }
 }
 
 
