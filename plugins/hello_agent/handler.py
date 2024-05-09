8ee45032eeed597d8b069f1fea4a58cc882e6a8c
Martin Mark <martin@promptpanel.com>
2024-05-09 14:22:09 -0400

    Updated sample agent to work

--- plugins/hello_agent/handler.py
+++ plugins/hello_agent/handler.py
@@ -1,7 +1,11 @@
+import os
+import json
 import logging
+import litellm
 import time
-from django.http import JsonResponse, StreamingHttpResponse
 from panel.models import File, Message, Panel, Thread
+from django.http import JsonResponse, StreamingHttpResponse
+from unstructured.partition.auto import partition
 
 logger = logging.getLogger("app")
 
@@ -18,24 +22,43 @@ def file_handler(file, thread, panel):
 
 
 def file_stream(file, thread, panel):
+    ## Function:
+    ## 1. Get settings.
+    ## 2. Parse file and save to .txt file.
+    ## 3. Enrich file metadata with token_count.
+
     try:
-        yield "File processing..."
-        filepath_split = file.filepath.split("/")[-1]
-        file_message_status = Message(
-            content="<strong>{filepath_split}</strong> uploaded and processed successfully. The document context will now be available for use in the chat.".format(
-                filepath_split=filepath_split
-            ),
-            thread=thread,
-            panel=panel,
-            created_by=file.created_by,
-            metadata={
-                "sender": "file_upload",
-                "file_id": file.id,
-                "file_path": file.filepath,
-            },
+        ## ----- 1. Get settings.
+        logger.info("** 1. Get settings.")
+        completion_model = "gpt-3.5-turbo"
+
+        ## ----- 2. Parse file and save to .txt file.
+        logger.info("** 2. Parse file and save to .txt file.")
+        yield "Parsing file to text..."
+        elements = partition(filename=file.filepath, strategy="fast")
+        output_filepath = file.filepath + ".txt"
+        with open(output_filepath, "w", encoding="utf-8") as output_file:
+            for element in elements:
+                output_file.write(str(element) + "\n")
+
+        ## ----- 3. Enrich file metadata with token_count / text_file_path.
+        logger.info("** 2. Enrich file metadata with token_count / text_file_path.")
+        yield "Counting tokens..."
+        with open(output_filepath, "r", encoding="utf-8") as input_file:
+            output_text = input_file.read()
+        output_text_formatted = f"{file.filename} Context:\n {output_text} \n\n"
+        new_file = [{"role": "user", "content": output_text_formatted}]
+        token_count = litellm.token_counter(model=completion_model, messages=new_file)
+        file.meta = json.loads(file.meta)
+        file.meta.update(
+            {"token_count": token_count, "text_file_path": output_filepath}
         )
-        file_message_status.save()
+        file.save()
+        yield "File upload and parsing complete..."
     except Exception as e:
+        logger.info("** Upload failed")
+        yield "File upload and parsing failed..."
+        file.delete()
         logger.error(e, exc_info=True)
 
 
